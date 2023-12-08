9376a01a6e77cc5790a2abf2ddba4018a2fc739d
ensar <ensarsagkal@gmail.com>
2023-12-08 13:00:05 +0300

    submit is handled

--- src/components/AddModal.jsx
+++ src/components/AddModal.jsx
@@ -5,11 +5,32 @@ import Modal from 'react-bootstrap/Modal';
 
 function AddModal({ doctor, onClose }) {
   const [show, setShow] = useState(true);
+  const[appointment,setAppointment]= useState({
+    doctorID:doctor.id,
+    patientName:"",
+    appointmentDate:""
+  })
+  const{doctorID,patientName,appointmentDate}=appointment
+  const handleName=(e)=>{
+    setAppointment({...appointment,[e.target.id]:e.target.value})
+  }
+  // console.log(appointment);
+  const handleDate=(e)=>{
+    setAppointment({...appointment,[e.target.id]:e.target.value})
+  }
+
+  console.log(appointment);
 
   const handleClose = () => {
     setShow(false);
     onClose();
   };
+  const handleAppointmentSubmit=(e)=>{
+    e.preventDefault()
+    alert("randevu kaydedildi")
+    onClose()
+   
+  }
 
   return (
     <Modal show={show} onHide={handleClose}>
@@ -17,12 +38,15 @@ function AddModal({ doctor, onClose }) {
         <Modal.Title><h4 className='text-danger'>Appointment For {doctor.name}</h4></Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <Form>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form onSubmit={handleAppointmentSubmit}>
+          <Form.Group className="mb-3" >
             <Form.Label><p className="fw-bold">Patient Name</p></Form.Label>
             <Form.Control
               type="text"
               placeholder="Enter your name"
+              onChange={handleName}
+              id='patientName'
+              value={patientName}
               autoFocus
             />
           </Form.Group>
@@ -32,20 +56,21 @@ function AddModal({ doctor, onClose }) {
           </Form.Label>
           <Form.Control
             type="datetime-local"
+            onChange={handleDate}
+            id='appointmentDate'
+            value={appointmentDate}
             
           />
         </Form.Group>
-        </Form>
-      </Modal.Body>
-      <Modal.Footer className='mx-auto'>
-       
-        <Button variant="success" type='submit' onClick={handleClose}>
+        <Button variant="success" type='submit'>
           Submit
         </Button>
         <Button variant="danger" onClick={handleClose}>
           Close
         </Button>
-      </Modal.Footer>
+        </Form>
+      </Modal.Body>
+    
     </Modal>
   );
 }
